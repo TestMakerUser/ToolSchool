bea2d092ab886c20c2d5474d74006be49a5d9633
valerii.zakharov <valerii.zakharov@smartteksas.com>
2021-09-17 16:42:41 +0300

    Add SceneService.

---
+++ Assets/Scripts/Services/Interfaces/ISceneService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SmartTek.ToolSchool.Services.Intefaces
+{
+    public interface ISceneService
+    {
+        void LoadScene(SceneService.SceneType scene, Action onComplete);
+    }
+}
